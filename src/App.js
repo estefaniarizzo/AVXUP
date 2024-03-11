28a62d53291785c875d8e8e500cf048138e85943
estefaniarizzo <tefa49395@gmail.com>
2024-03-11 15:49:29 -0500

    cambios terminados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import './App.css';
-import Vision from "./vision.png";
-import Mision from "./mision.png";
-import Logo from "./logo.png";
+
+import Logo from "./logo.jpg";
 import tecno from "./assets/tecno.jpg";
 import red from "./assets/red.jpg";
 import social from "./assets/social.jpg";
@@ -148,11 +147,11 @@ function App() {
   }, []);
   
   return (
-    <div>
+    <><div>
       <navbar>
-        <img src={Logo} alt="AVX UP Logo" />
+        <img src={Logo} alt="AVX UP Logo" /> <h6 className='h5'>Redes y Tecnologías</h6>
         <ul>
-          <li><a href="#servicios" >Servicios</a></li>
+          <li><a href="#servicios">Servicios</a></li>
           <li><a href="#quienes-somos">Quienes somos</a></li>
           <li><a href="#contacto">Contactanos</a></li>
         </ul>
@@ -160,30 +159,88 @@ function App() {
       <br />
       <br />
       <br />
-      
-      
+
+
       <Carrusel />
+      <br></br>
+
       <div id="servicios" className='servicios'>
         <h2>Nuestros Servicios</h2>
+        <hr></hr>
+        <p className='p-servicios'>Somos una empresa con personal altamente calificado con experiencia en telecomunicaciones,
+          nuestro portafolio de servicios ofrece la instalación de cableado estructurado, Fibra Óptica,
+          instalación de puntos de red certificados, integración de dispositivos de red como Switch, Routers,
+          traslado de datacenter, Instalación y organización de rack, mantenimientos preventivos y correctivos,
+          Soluciones de CCTV, Control de acceso, diseño y arquitectura de redes LAN, asesoramos para rediseñar su red LAN,
+          consultoría para redes Informáticas</p>
+        <p className='p-servicios'>AvxÜP Solutions SAS es una empresa de redes y telecomunicaciones que trabaja con materias
+          de alta calidad garantizando la durabilidad y rendimiento, se realiza acompañamiento al cliente
+          antes, durante y después de la implementación de nuestros servicios para garantizar una correcta
+          implementación y funcionalidad de la solución adquirida de nuestra empresa.</p>
         <main>
           <section>
             <div className="card">
-              <h4>UNIDAD INTEGRACIÓN DE CENTROS DE COMANDO Y CONTROL</h4>
-              <p>Brindamos soluciones avanzadas de tecnología para la implementación de salas de comando y control automatizadas.</p>
+              <h4>Cableado Estructurado y Fibra Óptica</h4>
+              <p>Nuestra especialidad radica en diseñar e implementar sistemas de cableado estructurado
+                y fibra óptica. Estos no solo garantizan una conectividad confiable, sino también una base sólida
+                para el crecimiento futuro de tu empresa. Imagina cada cable como un hilo que teje la red de posibilidades para tu éxito</p>
             </div>
             <div className="card">
-              <h4>UNIDAD DE TELECOMUNICACIONES</h4>
-              <p>Brindamos soluciones avanzadas de telecomunicaciones en los sitios más apartados del país.</p>
+              <h4>Instalación Puntos de Red Certificados</h4>
+              <p>No se trata solo de conectar cables. Creamos puntos de acceso estratégicos que cumplen con rigurosos
+                estándares de certificación. La señal fluye sin interrupciones, desde la sala de juntas hasta el rincón
+                más alejado. Tu empresa está respaldada por una infraestructura robusta.</p>
             </div>
             <div className="card">
-              <h4>UNIDAD INTELIGENCIA Y CIBERSEGURIDAD</h4>
-              <p>Brindamos soluciones tecnológicas avanzadas que buscan adelantarse a los movimientos de los criminales y terroristas.</p>
+              <h4>Integración de Dispositivos de Red</h4>
+              <p>Configuramos switches, routers y servidores con precisión quirúrgica.
+                Cada dispositivo es una pieza clave en el rompecabezas de tu red.
+                La sincronización perfecta garantiza una comunicación fluida y una experiencia sin tropiezos.</p>
             </div>
+          </section>
+        </main>
+        <main>
+          <section>
+            <div className="card">
+              <h4>Traslado de Datacenter y Organización de Racks</h4>
+              <p>Imagina mudar un edificio entero sin que nadie lo note. Eso es lo que hacemos.
+                Nuestro equipo se encarga de trasladar datacenters sin interrupciones. Además,
+                organizamos tus racks como un maestro ajedrecista: eficiencia y orden en cada movimiento.</p>
+            </div>
+            <div className="card">
+              <h4>Mantenimientos Preventivos y Correctivos</h4>
+              <p>No esperamos a que algo falle. Establecemos programas
+                de mantenimiento preventivo y correctivos para mantener tu red en su mejor forma.
+                Y si surge algún problema, actuamos con la mayor velocidad posible. Tu tranquilidad es nuestra prioridad.</p>
+            </div>
+            <div className="card">
+              <h4>Soluciones de CCTV y Control de Acceso</h4>
+              <p>La seguridad no es negociable. Nuestros sistemas de videovigilancia y control de acceso
+                protegen tus instalaciones las 24 horas. Tus empleados pueden
+                concentrarse en lo importante, sabiendo que están seguros.</p>
+            </div>
+          </section>
+        </main>
+        <main>
+          <section>
+            <div className="card">
+              <h4>Diseño y Arquitectura de Redes LAN</h4>
+              <p>Aquí es donde la magia sucede. Creamos redes de área local eficientes y seguras desde cero.
+                La topología, la segmentación y la redundancia son nuestro lienzo. Cada decisión está guiada por
+                la excelencia de profesionales experimentados</p>
+            </div>
+            <div className="card">
+              <h4>Rediseño de Redes Existentes</h4>
+              <p>A veces, incluso las redes necesitan un cambio de imagen. Te asesoramos para optimizar el rendimiento y la seguridad.
+                Si tu red es lenta y retrasa los procesos demora una eternidad en cargar una página, un documento, déjalo en nuestras manos,
+                evaluamos, diseñamos e implementamos la solución a la medida</p>
+            </div>
+
           </section>
         </main>
         <br />
       </div>
-<br></br>
+      <br></br>
       <div class="section-counter paralax-mf bg-image">
         <div class="overlay-mf"></div>
         <div class="container position-relative">
@@ -224,38 +281,119 @@ function App() {
           </div>
         </div>
       </div>
-
-      <h2 id="quienes-somos">Quienes somos</h2>
-      <div className='App'>
+      <div id="quienes-somos" className='servicios'>
+        <h2>Quienes somos</h2>
+        <hr></hr>
+        <p className='p-servicios'>AvxÜP Solutions SAS.  Es una empresa colombiana que ofrece soluciones integrales en el campo de las redes y las telecomunicaciones. Nuestra misión es brindar a nuestros clientes servicios de calidad, eficiencia y seguridad, adaptados a sus necesidades y expectativas. Contamos con un equipo de profesionales altamente cualificados y con amplia experiencia en el sector, que se encargan de diseñar, instalar y mantener sistemas de cableado estructurado, fibra óptica, equipos de comunicaciones como switch, routers y otros dispositivos relacionados con las redes. Nuestros valores son la innovación, el compromiso, la responsabilidad y la satisfacción del cliente. Nuestra visión es ser una empresa líder y referente en el mercado de las redes y las telecomunicaciones, ofreciendo soluciones a medida, personalizadas y de vanguardia. Si quieres saber más sobre nosotros, puedes contactarnos a través de nuestro formulario web, nuestro correo electrónico o nuestro teléfono. Estaremos encantados de atenderte y de ofrecerte la mejor solución para tu proyecto.</p>
         <main>
-          <section className='course-list'>
-            <CourseCard
-              description="Conectar a personas de todo el mundo, facilitando la creación de comunidades auténticas, el intercambio de ideas y experiencias, y promoviendo la inclusión y el entendimiento mutuo a través de una plataforma segura y fácil de usar."
-              imageSrc={Mision}
-            />
-            <CourseCard
-              description="Nos esforzamos por ser la plataforma de redes sociales líder a nivel mundial, donde la diversidad es celebrada, las voces son escuchadas y donde cada usuario puede encontrar un espacio para expresarse libremente, conectarse con otros y contribuir positivamente al mundo."
-              imageSrc={Vision}
-            />
+          <section>
+            <div className="card">
+              <h4>MISION</h4>
+              <p>En AvxÜP, nuestra misión es liderar el mercado en soluciones integrales de infraestructura de redes, especializándonos en la instalación de cableado estructurado, puntos de red certificados y Fibra Óptica. Nos comprometemos a la integración experta de equipos de red, como routers y switches, para proporcionar a nuestros clientes sistemas de comunicación cohesivos y eficientes. Nuestro enfoque está en la calidad, la innovación y el servicio personalizado, asegurando que cada solución sea adaptada a las necesidades específicas y contribuya al éxito tecnológico de nuestros clientes.</p>
+            </div>
+            <div className="card">
+              <h4>VISION</h4>
+              <p>Nuestra visión es ser el referente en diseño, implementación y mantenimiento de redes, impulsando la innovación tecnológica y la eficiencia operativa. Nos proyectamos como el aliado estratégico esencial para el éxito tecnológico de nuestros clientes, ofreciendo asesorías personalizadas y soluciones que se anticipan a las necesidades del mercado.</p>
+            </div>
+
           </section>
         </main>
+
+      </div>
+      <div className='servicios'>
+        <h2>Valores</h2>
+        <hr></hr>
+
+        <main>
+          <section>
+            <div className="card">
+              <h4>Calidad</h4>
+              <p>Nos comprometemos con la excelencia en cada servicio y producto que ofrecemos</p>
+            </div>
+            <div className="card">
+              <h4>Confiabilidad</h4>
+              <p>Garantizamos soluciones seguras y efectivas, construyendo una base sólida de confianza con nuestros clientes</p>
+            </div>
+            <div className="card">
+              <h4>Innovación</h4>
+              <p>Estamos en constante búsqueda de avances tecnológicos para mejorar nuestras ofertas y procesos.</p>
+            </div>
+
+          </section>
+
+        </main>
+        <main>
+          <section>
+            <div className="card">
+              <h4>Servicio al Cliente</h4>
+              <p>Priorizamos las necesidades de nuestros clientes, proporcionando atención detallada y personalizada</p>
+            </div>
+            <div className="card">
+              <h4>Trabajo en Equipo</h4>
+              <p>Valoramos la colaboración interna y externa para lograr los mejores resultadoss</p>
+            </div>
+
+
+          </section>
+        </main>
+      </div>
+      <div className='servicios'>
+        <h2>Objetivos</h2>
+        <hr></hr>
+
+        <main>
+          <section>
+            <div className="card">
+
+              <p>Ampliar nuestra gama de servicios para incluir las últimas innovaciones en Fibra Óptica y tecnología de redes</p>
+            </div>
+            <div className="card">
+
+              <p>Alcanzar la excelencia en la satisfacción del cliente a través de un soporte técnico superior y asesorías especializadas</p>
+            </div>
+            <div className="card">
+
+              <p>Invertir en el desarrollo profesional de nuestro equipo para mantenernos a la vanguardia en conocimientos y habilidades técnicas</p>
+            </div>
+            <div className="card">
+
+              <p>Implementar prácticas de sostenibilidad en todas nuestras operaciones para contribuir al cuidado del medio ambiente</p>
+            </div>
+          </section>
+
+        </main>
+
       </div>
       <div id="contacto">
         <h2>Contactanos</h2>
         <ContactForm />
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000b76" fill-opacity="1" d="M0,192L24,181.3C48,171,96,149,144,149.3C192,149,240,171,288,149.3C336,128,384,64,432,48C480,32,528,64,576,106.7C624,149,672,203,720,213.3C768,224,816,192,864,181.3C912,171,960,181,1008,181.3C1056,181,1104,171,1152,165.3C1200,160,1248,160,1296,144C1344,128,1392,96,1416,80L1440,64L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path></svg>
+      <br></br>
     </div>
+    <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-info">
+            <p>AvxÜP Solutions SAS</p>
+            <p>NIT: 901788132-4</p>
+            <p>Calle 16CBIS #98ª-97 Bogotá D.C</p>
+          </div>
+          <div className="footer-contact">
+            <p>Correo: avxupsas@outlook.es</p>
+            <p>Celular: 3132328996</p>
+          </div>
+          <div className="footer-social">
+            <p>Facebook: AvxÜP Solutions SAS</p>
+            <p>TikTok: avxupsolutions</p>
+            <p>X:  @AvxupSAS</p>
+            <p>LinkedIn: AvxÜP Solutions SAS</p>
+            <p>Instagram: avxupSolutions</p>
+          </div>
+        </div>
+      </footer></>
+
   );
 }
 
-function CourseCard({ title, description, imageSrc }) {
-  return (
-    <div className="course-card">
-      <img src={imageSrc} alt={title} />
-      <p>{description}</p>
-    </div>
-  );
-}
+
 
 export default App;
